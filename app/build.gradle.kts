96fc7989a79e197700d669de69b3b1dbe10a0872
acmiln <cddlsjy@163.com>
2026-04-14 17:59:55 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
 
     // Jetpack Compose
-    implementation(platform("androidx.compose:compose-bom:2025.12.00"))
+   implementation(platform("androidx.compose:compose-bom:2024.12.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
